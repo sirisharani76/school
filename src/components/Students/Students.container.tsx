7c9458012247077a99711edd76a3c2120f8bf83f
sirisharani76 <sirisharani76@gmail.com>
2022-10-03 16:10:42 +0100

    changed to Rama

--- src/components/Students/Students.container.tsx
+++ src/components/Students/Students.container.tsx
@@ -1,11 +1,16 @@
 import { useCallback, useEffect, useState } from "react";
-import { deleteStudentById, fetchStudentData, fetchStudentDataAsync, fetchStudentDataAxios } from "./Student.service";
+import {
+  deleteStudentById,
+  fetchStudentData,
+  fetchStudentDataAsync,
+  fetchStudentDataAxios,
+} from "./Student.service";
 import Students from "./Students";
 import { IStudent, IStudents } from "./Students.reducer";
 
 const StudentsContainer = () => {
   const [students, setStudents] = useState<IStudent[]>([
-    { id: "101", name: "Laxmi", age: 24, marks: 45 },
+    { id: "101", name: "Rama", age: 24, marks: 45 },
     { id: "102", name: "Vani", age: 26, marks: 48 },
     { id: "103", name: "Ravi", age: 29, marks: 45 },
   ]);
@@ -13,13 +18,16 @@ const StudentsContainer = () => {
     fetchStudentDataAxios().then((students) => setStudents(students));
   }, []);
 
-  const onDeleteStudent=useCallback((id:string)=>{
-    deleteStudentById(id).then(({id})=>setStudents(students))
-  },[])
+  const onDeleteStudent = useCallback((id: string) => {
+    deleteStudentById(id).then(({ id }) => setStudents(students));
+  }, []);
 
   return (
     <>
-      <Students students={students} onDeleteStudent={onDeleteStudent} ></Students>
+      <Students
+        students={students}
+        onDeleteStudent={onDeleteStudent}
+      ></Students>
     </>
   );
 };
